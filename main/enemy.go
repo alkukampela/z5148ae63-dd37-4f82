e0d6933b557866014de92021c8bf3ae56c28e61f
Risto Kuusterä <risto.kuustera@gofore.com>
2018-04-14 14:26:53 +0300

    moving boxes are awesome

--- main/enemy.go
+++ main/enemy.go
@@ -1,3 +1,18 @@
+components {
+  id: "enemy"
+  component: "/main/enemy.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
